18c6f52a2c3fb7219d2cf594023f7351513f200f
jentleman <abramiktaras@gmail.com>
2022-07-18 04:43:22 +0300

    Add files via upload

--- show_joke.js
+++ show_joke.js
@@ -1,21 +1,23 @@
-window.onload = function() {
-    document.getElementById('tell_me').style.display = "none";
-   };
-   function show_answer(event)
-   {
-       for(var item of document.querySelectorAll("input"))
-       {
-           item.setAttribute('disabled', '');
-       }
-       const radio_name = event.srcElement.getAttribute('id');
-       const label = document.querySelector("label[for='"+radio_name+"']");
-       if (radio_name == 'btnradio4')
-       {
-           label.setAttribute('class', label.getAttribute('class')+' right');
-       }
-       else
-       {
-           label.setAttribute('class', label.getAttribute('class')+' wrong');
-       }
-       document.getElementById('tell_me').style.display = 'block';
-   }
+function show_answer(event)
+{
+    for(var item of document.querySelectorAll("input"))
+    {
+        item.setAttribute('disabled', '');
+    }
+    const radio_id = event.srcElement.getAttribute('id');
+    const label = document.querySelector("label[for='"+radio_id+"']");
+    const links = ['unshaved.html', 'grandson.html', 'oldman.html', 'tideman.html'];
+    document.getElementById('show-btn').setAttribute('href', links[radio_id - 1]);
+    if (radio_id === '4')
+    {
+        label.setAttribute('class', label.getAttribute('class')+' right');
+        document.getElementById('show-btn').innerHTML ='Tell me!';
+    }
+    else
+    {
+        label.setAttribute('class', label.getAttribute('class')+' wrong');
+        document.getElementById('show-btn').innerHTML ='Show me!';
+    }
+
+    document.getElementById('tell_me').style.display = 'block';
+}
